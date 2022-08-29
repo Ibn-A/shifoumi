aa0728fbce1929b31e23fd957f80f11a2346a4d4
ibn-A <ibn_ali@yahoo.com>
2022-08-29 12:22:07 +0200

    Update getComputerChoice function which randomly return either 'rock','paper' or 'scissor'.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const choices = ["rock", "paper","scissor"];
 
 function getComputerChoice(){
    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-   console.log(randomChoice);
+   return randomChoice;
 }
 
 getComputerChoice();
+console.log(getComputerChoice())
